aec699a50d233cbfc3909fe52352a723779d5fff
melabreu <melissa.abreu84@gmail.com>
2025-11-16 19:56:25 -0500

    Configure STATICFILES_STORAGE and static directory

--- Echoes/settings.py
+++ Echoes/settings.py
@@ -162,6 +162,8 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 USE_L10N = True
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
